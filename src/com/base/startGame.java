acfaef7808999f250be94902e896ba758f6e1d75
Alex Byrd <soccer43802@gmail.com>
2018-02-12 21:02:42 -0600

    Initilization of the project

---
+++ src/com/base/startGame.java
@@ -0,0 +1,22 @@
+package com.base;
+
+/**
+ * @title startGame
+ * @author Alexander Byrd
+ * Date Created: February 12, 2018
+ * 
+ * Description:
+ * Starts the game. Nothing much else to say.
+ *
+ */
+public class startGame {
+
+	public startGame() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
